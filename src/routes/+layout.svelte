b0a45bf1c89241243c388cfbaa5dfdfaf1e89112
Stephan Pitois <spitois@gmail.com>
2022-12-13 17:16:34 -0500

    Listen to TOTORO_WAS_CLICKED + smaller images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,55 @@
 <script>
+  // @ts-nocheck
+
   import Header from '$lib/header/Header.svelte';
   import '../app.css';
   import { count } from '$lib/stores.js';
+  import { onMount, onDestroy } from 'svelte';
+
+  let totoroEvents = [];
+
+  // Use this to listen to events triggered by other micro-frontends:
+  const listener = ({ detail }) => {
+    console.log('Layout heard Totoro.');
+    totoroEvents = [...totoroEvents, {
+      ts: new Date(),
+      message: detail.message
+    }];
+  };
+
+  onMount(async () => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('TOTORO_WAS_CLICKED', listener);
+    }
+  });
+
+  onDestroy(async () => {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('TOTORO_WAS_CLICKED', listener);
+    }
+  });
 </script>
 
 <div class="wrapper">
   <div class="box header"><Header /></div>
   <div class="box sidebar">
     Sidebar
-    
-    <p>The App Shell has its own stores and cannot access other MFEs' stores. For example, click
-    count store remains 0 in the sidebar while it can increase in MFE 1. Clicks: {$count}</p>
+    <p>
+      The App Shell has its own stores and cannot access other MFEs' stores. For example, click
+      count store remains 0 in the sidebar while it can increase in MFE 1. Clicks: {$count}
+    </p>
+
+    Events
+    {#if totoroEvents.length}
+      {#each totoroEvents as totoroEvent}
+        <p>
+          {(new Date(totoroEvent.ts)).toLocaleTimeString()}<br>
+          {totoroEvent.message}
+        </p>
+      {/each}
+    {:else}
+      <p>No events yet. Click on Totoro's first child component.</p>
+    {/if}
   </div>
   <div class="box content">
     <main>
