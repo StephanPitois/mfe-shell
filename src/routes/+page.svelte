856b321a4ebc2b0214627d3fb4b08803e0e68688
Stephan Pitois <spitois@gmail.com>
2022-12-18 08:51:15 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <section class="flex flex-col gap-y-5 p-b5 pl-5 pr-20">
   <h1>Micro-Frontend Demo</h1>
   <p>
-    A simple Svelte-based micro-frontend demo. Choose Page 1 or Page 2 from the navigation menu to
+    A simple Svelte-based micro-frontend demo. Choose "Products" or "Another Page" from the navigation menu to
     see micro-frontends in action.
   </p>
   <p>
