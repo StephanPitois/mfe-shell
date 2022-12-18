9eee1a390e3c9b6e4a94ce3286fb2a7a9ddb73a5
Stephan Pitois <spitois@gmail.com>
2022-12-18 07:25:26 -0500

    SvelteKit 1.0 upgrade

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-// import UnoCSS from 'unocss/vite';
+import UnoCSS from 'unocss/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [
-    // UnoCSS({
-    //   mode: 'svelte-scoped',
-    // }),
+    UnoCSS({
+      mode: 'svelte-scoped',
+    }),
     sveltekit()
   ],
   define: {
