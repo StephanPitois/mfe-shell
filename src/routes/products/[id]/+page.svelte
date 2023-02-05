16e589face3344b3c43f7b67cc282481970094c6
Stephan Pitois <spitois@gmail.com>
2023-02-05 13:08:06 -0500

    Add product detail page

---
+++ src/routes/products/[id]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  // @ts-nocheck
+  import RemoteModule from '$lib/micro-frontends/RemoteModule.svelte';
+</script>
+
+<svelte:head>
+  <title>Product - MFE Demo</title>
+  <meta name="description" content="Product - MFE Demo" />
+</svelte:head>
+
+<div class="gap-10">
+  <!-- svelte-ignore missing-declaration -->
+  <RemoteModule remote="remote-app-1" component={() => remote_app_1.Product} />
+</div>
