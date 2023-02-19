6b71833c05d7d7e0687d6d599178c6c1ad42675e
Stephan Pitois <spitois@gmail.com>
2023-02-19 01:57:00 -0500

    Show error for dyn mod import

--- src/lib/micro-frontends/RemoteModule.svelte
+++ src/lib/micro-frontends/RemoteModule.svelte
@@ -14,19 +14,28 @@
   const id = `mfe-${crypto.randomUUID()}`;
 
   let loaded = false;
+  let error = '';
 
   onMount(async () => {
     // @ts-ignore
     const url = mfe_importmap[remote];
-    import(/* @vite-ignore */ url).then((module) => {
-      const constructor = module[component];
-      new constructor({
-        target: document.getElementById(id)
+    const target = document.getElementById(id);
+    import(/* @vite-ignore */ url)
+      .then((module) => {
+        const constructor = module[component];
+        new constructor({ target });
+        error = '';
+        loaded = true;
+      })
+      .catch((err) => {
+        error = err.message;
       });
-      loaded = true;
-    });
   });
 </script>
+
+{#if error}
+  <article>{error}</article>
+{/if}
 
 <!-- <div class:hidden={!loaded} in:fly={{ y: 100, duration: 1000 }}> -->
 <div class:hidden={!loaded}>
@@ -35,3 +44,9 @@
   </div>
   <div {id} class="mfeCard" />
 </div>
+
+<style>
+  article {
+    border-color: #bf2e3a;
+  }
+</style>
